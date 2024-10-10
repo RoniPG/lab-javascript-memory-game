9e5bab72d46a00dd45cd240eac0c1fd45bc44acb
RoniPG <ronigmn@gmail.com>
2024-10-10 18:08:14 +0200

    Iteration 2.2: The class methods (1 fail in shuffleCards())

--- src/memory.js
+++ src/memory.js
@@ -7,12 +7,37 @@ class MemoryGame {
     this.pairsGuessed = 0;
   }
 
-  shuffleCards() {
+  shuffleCards(cards) {
     // ... write your code here
+    // Método de Fisher-Yates
+    if (!cards || cards.length <2) {
+      return undefined;
+    } else {
+      let originalOrder = [...cards]; // Copia del array original para comparación
+      let isSameOrder;
+
+      do {
+        for (let i = cards.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));  // Genera un número aleatorio entre 0 y i
+          [cards[i], cards[j]] = [cards[j], cards[i]];    // Intercambia array[i] con array[j]
+        }
+         // Comparamos si el nuevo orden es igual al original
+        isSameOrder = originalOrder.every((card, index) => card === cards[index]);
+      } while (isSameOrder); // Si el orden no cambió, vuelve a mezclar
+
+      return cards;    
+    }
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked ++;
+    if (card1 === card2) {
+      this.pairsGuessed ++;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
